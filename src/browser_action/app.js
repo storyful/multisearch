8d709998fea0b0dca3039e2fcf545dc193188406
Bruno Tarmann <bruno@tarmann.com.br>
2014-12-04 12:56:58 +0000

    code cleanup and extract methods from app.js

--- src/browser_action/app.js
+++ src/browser_action/app.js
@@ -1,72 +1,99 @@
 $(document).ready(function() {
 
-	// mixpanel.track("MultiSearch shown");
-		
-	$('#keyword').val(localStorage['keyword']);
+  var bindEvents = function(){
+    $("input[type='checkbox']").change(saveSearchOptions);
+    $('#keyword').change(saveKeyword);
+    $('form').submit(search);    
+  }
 
-	// Remember checkbox choices between sessions
-	$.each($("input[type='checkbox']"), function(index, ele) {
-		if (localStorage[$(ele).attr('id')] == undefined) {
-			if ($(ele).is(':checked')) {
-				localStorage[$(ele).attr('id')] = 'true';
-			} else {
-				localStorage[$(ele).attr('id')] = 'false';
-			}
-		}
+  var initialize = function(){
+    console.log('hello')
 
-		if (localStorage[$(ele).attr('id')] == 'false') {
-			$(ele).removeAttr('checked');
-		} else {
-			$(ele).attr('checked', 'checked');
-		}
-	});
+    // mixpanel.track("MultiSearch shown");
+    $('#keyword').val(localStorage['keyword']);
 
-	// Store checkbox choice
-	$("input[type='checkbox']").change(function(e) {
-		$.each($("input[type='checkbox']"), function(index, ele) {
-			if ($(ele).is(':checked'))	{
-				localStorage[$(ele).attr('id')] = 'true';
-			} else {
-				localStorage[$(ele).attr('id')] = 'false';
-			}
-		});
-	});
+    // Remember checkbox choices between sessions
+    $.each($("input[type='checkbox']"), function(index, ele) {
+      if (localStorage[$(ele).attr('id')] == undefined) {
+        if ($(ele).is(':checked')) {
+          localStorage[$(ele).attr('id')] = 'true';
+        } else {
+          localStorage[$(ele).attr('id')] = 'false';
+        }
+      }
 
-	// Remember keyword between sessions
-	$('#keyword').change(function() {
-		localStorage['keyword'] = $(this).val();
-	});
+      if (localStorage[$(ele).attr('id')] == 'false') {
+        $(ele).removeAttr('checked');
+      } else {
+        $(ele).attr('checked', 'checked');
+      }
+    });
+  }  
 
-	$('form').submit(function(e) {
+  var search = function(e) {
     e.preventDefault();
 
-    var k = $('#keyword').val();
+    console.log('search');
+
+    var k = $('#keyword').val(),
+        tabs = [];
 
     if (k.replace(/ /g, '').length == 0) {
-			$('#keyword').focus();        	
+      $('#keyword').focus();
     } else {
-    	// mixpanel.track("MultiSearch requested"); //#NOTE: Only tracks that a search happened, not what the search was for.
-    	
-    	// To open more than 3 tabs at once in Chrome requires trickery.
-    	var code_block = '';
-			$.each($("input[type='checkbox']:checked"), function(index, ele) {
-				var query = k;
-				if ($(ele).attr('data-search-modifier') != 'undefined') {
-					var modifier = new RegExp($(ele).attr('data-search-modifier'), 'g');
-					query = k.replace(modifier, '');
-				}
-				var search_url = $(ele).attr('data-search-url').replace('{{k}}', encodeURIComponent(query));
-				if (index == 0) {
-					code_block = 'document.location = "' + search_url + '"; ';
-				} else {
-					code_block = code_block + 'window.open("' + search_url + '");';
-				}
-    	});
+      // mixpanel.track("MultiSearch requested");
+      // #NOTE: Only tracks that a search happened, not what the search was for.
+      $.each($("input[type='checkbox']:checked"), function(index, el) {
+        tabs.push( getSearchUrl(el, k) );
+      });
 
-			// Cannot executeScript in chrome:// tabs so must open real URL.
-    	chrome.tabs.create({url: 'http://storyful.com?utm=multisearch', active: false}, function(tab) {
-    		chrome.tabs.executeScript(tab.id, {runAt: 'document_start', code: code_block});
-	    });
-		}
-	});
+      openTabs(tabs);
+    }
+  }
+
+  var getSearchUrl = function(el, query){
+    if ($(el).attr('data-search-modifier') != 'undefined') {
+      var modifier = new RegExp($(el).attr('data-search-modifier'), 'g');
+      query = query.replace(modifier, '');
+    }
+
+    return $(el).attr('data-search-url').replace('{{k}}', encodeURIComponent(query));
+  }
+  
+  var openTabs = function(urls){
+    var script = [];
+    
+    // To open more than 3 tabs at once in Chrome requires trickery.
+    for(i = 0; i < urls.length; i++){
+      script.push((i == 0) ?
+        'document.location = "' + urls[i] + '"; ' :
+        'window.open("' + urls[i] + '");');
+    }
+
+    console.log(script.join(''));
+
+    // Cannot executeScript in chrome:// tabs so must open real URL.
+    chrome.tabs.create({ url: 'http://storyful.com?utm=multisearch', active: false }, function(tab) {
+      chrome.tabs.executeScript(tab.id, { runAt: 'document_start', code: script.join('') });
+    });
+  }
+
+  var saveKeyword = function(el) {
+    localStorage['keyword'] = $(el).val();
+  }
+
+  var saveSearchOptions = function(e) {
+    $.each($("input[type='checkbox']"), function(index, ele) {
+      if ($(ele).is(':checked'))  {
+        localStorage[$(ele).attr('id')] = 'true';
+      } else {
+        localStorage[$(ele).attr('id')] = 'false';
+      }
+    });
+  }
+
+  bindEvents();
+  
+  initialize();
+
 });
